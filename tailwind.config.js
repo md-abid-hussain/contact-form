efa1c140471718d1825e5bbbd6d99a4329e2c8e1
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-06-04 23:30:46 +0530

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors:{
-      "primary-green-200":"#dff1e7",
-      "primary-green-600":"#0c7d69",
-      "primary-red":"#d73c3c",
-      "neutral-grey-500":"#87a3a6",
-      "neutral-grey-900":"#2b4246",
+    extend: {
+      colors: {
+        "primary-green-200": "#dff1e7",
+        "primary-green-600": "#0c7d69",
+        "primary-red": "#d73c3c",
+        "neutral-grey-500": "#87a3a6",
+        "neutral-grey-900": "#2b4246",
+      },
     },
-    extend: {},
   },
   plugins: [],
-}
+};
